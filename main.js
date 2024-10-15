4d24088bd623be3a6fab35c45c0b5c73c235d45a
ndchieu <mylover21072000@gmail.com>
2024-10-15 21:04:41 +0700

    first commit

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 const chatBody = document.querySelector(".chat-body");
 const messageInput = document.querySelector(".message-input");
 const sendMessageButton = document.querySelector("#send-message");
+const fileInput = document.querySelector("#file-input");
+const fileUploadWrapper = document.querySelector(".file-upload-wrapper");
+const fileCancelButton = document.querySelector("#file-cancel");
+const chatbotToggle = document.querySelector("#chatbot-toggle");
+const closeChatbot = document.querySelector("#close-chatbot");
 
 // API Key của Google Cloud Platform
 const API_KEY = 'AIzaSyApTa59Kl2rgiGsTS2Vjn7TUE166kP6iQ8';
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
 
 const userData = {
-    message: null
+    message: null,
+    file: {
+        data: null,
+        mime_type: null
+    }
 }
+
+const initialInputHeight = messageInput.scrollHeight;
 
 // Tạo phần tử thông báo với các lớp động và trả về nó
 const createMessageElement = (content, ...classes) => {
@@ -21,28 +32,32 @@ const createMessageElement = (content, ...classes) => {
 // Xử lý phản hồi của bot
 const generateBotResponse = async(incomingMessageDiv) => {
     const messageElement = incomingMessageDiv.querySelector(".message-text");
-    const requesOptions = {
+    const requestOptions = {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
             contents: [{
-                parts: [{ text: userData.message }]
+                parts: [{ text: userData.message }, ...(userData.file.data ? [{ inline_data: userData.file }] : [])]
             }]
         })
     }
 
 
+
     try {
-        const response = await fetch(API_URL, requesOptions);
+        const response = await fetch(API_URL, requestOptions);
         const data = await response.json();
         if (!response.ok) throw new Error(data.error.message);
         const apiResponseText = data.candidates[0].content.parts[0].text.replace(/\*\*(.*?)\*\*/g, "$1").trim();
         messageElement.innerText = apiResponseText;
     } catch (error) {
+        //Xử lý lỗi trong phản hồi API
         console.log(error);
         messageElement.innerText = error.message;
         messageElement.style.color = "#ff0000";
     } finally {
+        // Đặt lại dữ liệu tệp của người dùng, xóa chỉ báo suy nghĩ và cuộn trò chuyện xuống cuối
+        userData.file = {};
         incomingMessageDiv.classList.remove("thinking");
         chatBody.scrollTo({ top: chatBody.scrollHeight, behavior: "smooth" });
 
@@ -51,11 +66,12 @@ const generateBotResponse = async(incomingMessageDiv) => {
 
 // Xử lý tin nhắn gửi đi của người dùng
 const handleOutgoingMessage = (e) => {
-    e.preventDefault();
-    userData.message = messageInput.value.trim();
-    messageInput.value = "";
+        e.preventDefault();
+        userData.message = messageInput.value.trim();
+        messageInput.value = "";
+        fileUploadWrapper.classList.remove("file-uploaded")
 
-    const messageContent = `<div class="message-text"></div>`;
+        const messageContent = `<div class="message-text"></div> ${userData.file.data ? `<img src= "data:${userData.file.mime_type};base64,${userData.file.data}" class ="attachment" />` : ""}`;
     const outgoingMessageDiv = createMessageElement(messageContent, "user-message");
     outgoingMessageDiv.querySelector(".message-text").textContent = userData.message;
     chatBody.appendChild(outgoingMessageDiv);
@@ -90,4 +106,64 @@ messageInput.addEventListener("keydown", (e) => {
     }
 });
 
+// Adjust input field height dynamically
+messageInput.addEventListener("input", () => {
+    messageInput.style.height = `${initialInputHeight}px`;
+    messageInput.style.height = `${messageInput.scrollHeight }px`;
+    document.querySelector(".chat-form").style.borderRadius =
+     messageInput.scrollHeight > initialInputHeight ? "15px" : "32px"
+});
+
+// Handle chọn và tải file
+fileInput.addEventListener("change", () => {
+    const file = fileInput.files[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.onload = (e) => {
+        fileUploadWrapper.querySelector("img").src = e.target.result;
+        fileUploadWrapper.classList.add("file-uploaded")
+        // Lưu dữ liệu file thành base64 string và lưu vào userData.file.data
+        const base64String = e.target.result.split(",")[1];
+        userData.file = {
+            data: base64String,
+            mime_type: file.type
+        }
+        fileInput.value = "";
+    }
+    reader.readAsDataURL(file)
+
+});
+
+//Cancel file upload
+fileCancelButton.addEventListener("click",() => {
+    userData.file = {};
+    fileUploadWrapper.classList.remove("file-uploaded")
+});
+
+// Emoji picker and handle emoji selection
+const picker = new EmojiMart.Picker({
+    theme :"light",
+    skinTonePosition: "none",
+    previewPosition: "none",
+    onEmojiSelect: (emoji) => {
+    const {selectionStart: start ,selectionEnd: end} = messageInput;
+    messageInput.setRangeText(emoji.native, start,end,"end");
+    messageInput.focus();
+},
+    
+    onClickOutside: (e) => {
+        if(e.target.id ==="emoji-picker"){
+            document.body.classList.toggle("show-emoji-picker");
+        }else {
+            document.body.classList.remove("show-emoji-picker");
+        }
+    }
+});
+
+document.querySelector(".chat-form").appendChild(picker);
+
 sendMessageButton.addEventListener("click", (e) => handleOutgoingMessage(e));
+document.querySelector("#file-upload").addEventListener("click", () => fileInput.click());
+chatbotToggle.addEventListener("click",() => document.body.classList.toggle("show-chatbot"));
+closeChatbot.addEventListener("click", () => document.body.classList.remove("show-chatbot"));
